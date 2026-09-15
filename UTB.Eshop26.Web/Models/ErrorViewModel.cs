324a7be737b8b1f2d06c4412387a119eb927190d
Tomas Vogeltanz <vogeltanz@utb.cz>
2026-09-15 10:49:22 +0200

    Initial commit
    + default ASP.NET Core MVC App was created (.NET 10)

---
+++ UTB.Eshop26.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UTB.Eshop26.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
