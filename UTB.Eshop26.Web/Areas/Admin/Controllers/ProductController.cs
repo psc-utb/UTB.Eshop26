e887d531c3280ae2928917b76e0c588ace75b078
Tomas Vogeltanz <tom.vogeltanz@gmail.com>
2026-09-21 13:52:29 +0200

    Oblasti (Areas) a routování
    + Přidání Area Admin (s vytvořením potřebných složek v projektu -> Areas, Areas/Admin, Areas/Admin/Controllers, Areas/Admin/Views, Areas/Admin/Views/Product)
    ~ Přidání routování na oblasti do Program.cs
    + Přidán ProductController v Area Admin s atributem propojujícím Controller a oblast
    + Přidána metoda Select do ProductController s návratem View
    + Přidáno View Select pro ProductController
    + Přidán odkaz na administraci produktů do Layoutu
    + Přidány/Zkopírovány soubory _ViewImports a _ViewStart do area Admin

---
+++ UTB.Eshop26.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.Eshop26.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Select()
+        {
+            return View();
+        }
+    }
+}
--- UTB.Eshop26.Web/Program.cs
+++ UTB.Eshop26.Web/Program.cs
@@ -33,6 +33,10 @@ app.UseAuthorization();
 app.MapStaticAssets();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
