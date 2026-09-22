67444d0c6a7f9cc32440a9e3ffde5ab954b69df3
Tomas Vogeltanz <vogeltanz@utb.cz>
2026-09-22 15:10:09 +0200

    ViewModels pro registrační a přihlašovací formulář
    + Vytvoření LoginViewModel a RegisterViewModel pro formuláře s registrací a přihlašováním

---
+++ UTB.Eshop.Application/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UTB.Eshop.Application.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Password { get; set; }
+
+        public bool LoginFailed { get; set; }
+    }
+}
---
+++ UTB.Eshop.Application/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UTB.Eshop.Application.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords don't match!")]
+        public string? RepeatedPassword { get; set; }
+    }
+}
