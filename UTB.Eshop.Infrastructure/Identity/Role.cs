bef6178c65e87b01308a3dc5e3fda4b5c65fbcde
Tomas Vogeltanz <vogeltanz@utb.cz>
2026-09-22 13:34:40 +0200

    Entity pro Identity
    + Vytvoření entit pro Identity: User (implementuje IUser), Role

---
+++ UTB.Eshop.Infrastructure/Identity/Role.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UTB.Eshop.Infrastructure.Identity
+{
+    /// <summary>
+    /// Our Role class which can be modified
+    /// </summary>
+    public class Role : IdentityRole<int>
+    {
+        public Role(string role) : base(role)
+        {
+        }
+
+        public Role() : base()
+        {
+        }
+    }
+}
---
+++ UTB.Eshop.Infrastructure/Identity/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UTB.Eshop.Domain.Entities.Interfaces;
+
+namespace UTB.Eshop.Infrastructure.Identity
+{
+    /// <summary>
+    /// Our User class which can be modified
+    /// </summary>
+    public class User : IdentityUser<int>, IUser<int>
+    {
+        public virtual string? FirstName { get; set; }
+        public virtual string? LastName { get; set; }
+    }
+}
